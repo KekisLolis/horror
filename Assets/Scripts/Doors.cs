48ac9cbe910cda17288f61df6bb26bcd2102bf94
KekisLolis <godyaev2@gmail.com>
2024-04-05 12:44:21 +0300

    Initial commit

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+    public AudioSource doorSound;
+
+    public bool inReach;
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            DoorOpens();
+        }
+        
+        else
+        {
+            DoorCloses();
+        }
+    }
+    
+    void DoorOpens ()
+    {
+        door.SetBool("open", true);
+        door.SetBool("close", false);
+        doorSound.Play();
+    }
+
+    void DoorCloses()
+    {
+        door.SetBool("open", false);
+        door.SetBool("close", true);
+    }
+}
